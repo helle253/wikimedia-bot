afa900025cdc4a347c5bc4e64a060cf931b0426d
hell253 <nathanhellbhoy@gmail.com>
2023-06-09 20:10:58 -0500

    1. use `serverless deploy` to deploy to aws 2. add resizing

--- update_lambda.sh
+++
@@ -1,6 +0,0 @@
-cd venv/lib/python3.10/site-packages
-zip -r9 ${OLDPWD}/lambda.zip .
-cd $OLDPWD
-zip -g lambda.zip lambda_function.py 
-zip -rg lambda.zip ./aws_helpers 
-# aws lambda update-function-code --function-name wikimedia-bot --zip-file fileb://lambda.zip

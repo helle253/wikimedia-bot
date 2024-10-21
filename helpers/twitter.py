d8ecb513131ce0a716ad336c48a4520e524efcee
helle253 <nathanhellbhoy@gmail.com>
2024-10-21 12:39:05 -0500

    reduce twitter image size to a maximum of 2048x2048

--- helpers/twitter.py
+++ helpers/twitter.py
@@ -2,7 +2,7 @@ import tweepy
 from boto3 import client
 from typing import Dict
 
-from helpers.images import get_image, to_bytes
+from helpers.images import fit_image_to_constraint, get_image, to_bytes
 from helpers.wikimedia import get_file_details
 
 ssm = client('ssm')
@@ -20,9 +20,7 @@ def post(file: Dict[str, any]) -> None:
   details = get_file_details(file['title'])
   description_url = details['descriptionurl']
   image = get_image(details['url'])
-  media_upload = api.simple_upload(f'image.{image.format}', file=to_bytes(image))
-  print('Uploaded media with ID: ', media_upload.media_id)
+  resized_image  = fit_image_to_constraint(image, 2048)
+  media_upload = api.simple_upload(f'image.{image.format}', file=to_bytes(resized_image))
   api.create_media_metadata(media_upload.media_id, alt_text=description_url)
-  print('Added alt text to upload: ', media_upload.media_id)
   client.create_tweet(media_ids=[media_upload.media_id])
-  print('created Tweet')

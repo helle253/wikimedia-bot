9eaffdc2961805a9af6d9ec97927ea20318e32a9
helle253 <nathanhellbhoy@gmail.com>
2024-10-21 11:13:29 -0500

    try posting to twitter?

--- helpers/twitter.py
+++ helpers/twitter.py
@@ -2,12 +2,13 @@ import tweepy
 from boto3 import client
 
 ssm = client('ssm')
-bearer_token = ssm.get_parameter(Name='/wikimedia_bot/twitter/bearer_token')['Parameter']['Value']
+consumer_key = ssm.get_parameter(Name='/wikimedia_bot/twitter/api_key')['Parameter']['Value']
+consumer_secret = ssm.get_parameter(Name='/wikimedia_bot/twitter/api_key_secret')['Parameter']['Value']
+access_token = ssm.get_parameter(Name='/wikimedia_bot/twitter/access_token')['Parameter']['Value']
+access_token_secret = ssm.get_parameter(Name='/wikimedia_bot/twitter/access_token_secret')['Parameter']['Value']
 
-auth = tweepy.OAuth2BearerHandler(bearer_token)
-
-api = tweepy.API(auth)
-client = tweepy.Client(bearer_token)
+client = tweepy.Client(consumer_key=consumer_key, consumer_secret=consumer_secret,
+                       access_token=access_token, access_token_secret=access_token_secret)
 
 def post(url: str) -> None:
-  print('Tweeted!')
+  client.create_tweet('Hello World!')

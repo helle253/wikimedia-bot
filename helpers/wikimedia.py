7f0e1afb7bc7d858b1396ca9e31b480eb9ee7ee9
helle253 <nathanhellbhoy@gmail.com>
2025-04-12 08:25:23 -0500

    implement find_non_posted_image
    i could have sworn i had implemented this lol, i dont know what happened lmao

--- helpers/wikimedia.py
+++ helpers/wikimedia.py
@@ -3,7 +3,9 @@ from typing import Dict, Union
 from random import randrange
 from datetime import timedelta, datetime
 
-# dynamodb = dynamodb.DynamoDBWrapper()
+from helpers.dynamodb import DynamoDBWrapper
+
+dynamodb = DynamoDBWrapper()
 
 def _random_time(
     start=datetime(2011,3,8,13),
@@ -31,7 +33,10 @@ def _find_non_posted_image(results) -> Union[None, any]:
     Returns nothing if all the results have already been posted.
     Otherwise, returns the id and title of an image that has not been posted.
   '''
-  return results[0]
+  for result in results:
+    if not dynamodb.is_already_posted(result['pageId']):
+      return result
+  return None
 
 ##
 # Returns a title and an ID, which can be used to query for the image itself.
